632ffa9a14144ef0d46181510a67684c0adfa9ed
asanchez365365 <saintxotic123@gmail.com>
2025-12-11 14:31:32 +0000

    loop practice

--- 3_looping_over_string.py
+++ 3_looping_over_string.py
@@ -1,6 +1,17 @@
 # Example Practice:
 # Ask the user for a word.
 # Use a for loop to print each letter on a new line.
+word = input("Please enter a word: ")
+
+for letter in word:
+    print(letter)
 
 # Challenge:
 # Count how many vowels are in the word.
+
+vowels = "aeiouAEIOU"
+count=0
+for letter in word:
+    if letter in vowels:
+        count += 1
+print("number of vowels,", count)
